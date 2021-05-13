3ab8a2ac328576150a8b1bca9e9537d82eeb3fcb
tobenna <tjlite81@gmail.com>
2021-05-13 10:30:53 -0400

    testing react native image animation feature

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import trumpet from './trumpet.svg';
+import trumpet from './images/trumpet.svg';
 import './App.css';
+import ImageSequence from 'react-native-image-sequence-2';
 
+const images = [
+  require('./images/trumpet.svg'),
+  require('./images/bass_drum.svg'),
+  require('./images/clarinet.svg'),
+  require('./images/guitar.svg'),
+  require('./images/french_horn.svg'),
+  require('./images/micro2.svg'),
+  require('./images/saxophone.svg'),
+  require('./images/violin.svg')
+]
+const centerIndex = Math.round(images.length / 2);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={trumpet} className="App-logo" alt="logo" />
+        {/* <img src={trumpet} className="App-logo" alt="logo" /> */}
+        <ImageSequence
+          images={images}
+          startFrameIndex={centerIndex}
+          style={{width: 50, height: 50}}
+          framesPerSecond={24}
+          loop={true}
+        />
         <p>
           Bander
         </p>
