780f0c35bdc14d6cd615a7bcd7ffdd8e803feeb6
tobenna <tjlite81@gmail.com>
2021-05-13 11:34:02 -0400

    reset to earlier commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import trumpet from './trumpet.svg';
+import trumpet from './images/trumpet.svg';
 import './App.css';
+// import ImageSequence from 'react-native-image-sequence';
 
+// const images = [
+//   require('./images/trumpet.svg'),
+//   require('./images/bass_drum.svg'),
+//   require('./images/clarinet.svg'),
+//   require('./images/guitar.svg'),
+//   require('./images/french_horn.svg'),
+//   require('./images/micro2.svg'),
+//   require('./images/saxophone.svg'),
+//   require('./images/violin.svg')
+// ]
+// const centerIndex = Math.round(images.length / 2);
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={trumpet} className="App-logo" alt="logo" />
-        <p>
+        {/* <ImageSequence
+          images={images}
+          startFrameIndex={centerIndex}
+          style={{width: 50, height: 50}}
+          framesPerSecond={24}
+          loop={true}
+        /> */}
+        <p> 
           Bander
         </p>
         {/* <a
