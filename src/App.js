19298b8b76eeb02fde2b12ba03e5c06b5af43a18
tobenna <tjlite81@gmail.com>
2021-05-12 22:56:54 -0400

    testing background color change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import trumpet from './trumpet.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={trumpet} className="App-logo" alt="logo" />
         <p>
           hello Shmerry.
         </p>
