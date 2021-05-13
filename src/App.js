ee83c5df8fb65742b17ba2d00a2386f6c8a53e33
tobenna <tjlite81@gmail.com>
2021-05-12 22:20:20 -0400

    testing text changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> hello Shmerry.
+          hello Shmerry.
         </p>
         {/* <a
           className="App-link"
