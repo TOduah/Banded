7de8d7fe7e61876f5d443a431d2f91aa78add820
tobenna <tjlite81@gmail.com>
2021-05-16 00:08:21 -0400

    switched instrument images

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,6 +1,6 @@
 import trumpet from './images/trumpet.svg';
 import bass_drum from './images/bass_drum.svg';
-import guiatr from './images/guitar.svg';
+import guitar from './images/guitar.svg';
 import saxophone from './images/saxophone.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react'
