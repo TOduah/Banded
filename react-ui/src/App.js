49a61c3c53845c692fa5389fcc38d28cfca482b1
tobenna <tjlite81@gmail.com>
2021-05-18 21:12:48 -0400

    using Helmet within header tag

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -32,6 +32,26 @@ function App() {
         <Helmet>
         <script src="./components/slide.js" type="text/jsx" />
       </Helmet>
+      {/* <script>
+        {
+          `var slideIndex = 0;
+          showSlides();
+          
+          function showSlides() {
+            var i;
+            var slides = document.querySelectorAll(".slide");
+            for (i = 0; i < slides.length; i++) {
+              slides[i].style.display = "none";  
+            }
+            slideIndex++;
+            if (slideIndex > slides.length) {slideIndex = 1}    
+          
+            slides[slideIndex-1].style.display = "block";  
+          
+            setTimeout(showSlides, 8000); 
+          } `
+        }
+      </script> */}
       </header>
     </div>
   );
