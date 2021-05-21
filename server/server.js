d3dcd8c7eb1287a3e0f4602e3acdf15d5bd35827
tobenna <tjlite81@gmail.com>
2021-05-21 08:54:52 -0400

    updated index.js with previous setup code

--- server/server.js
+++ server/server.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import cors from 'cors'
-const express = require("express");
-// const path = require('path');
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
-const axios = require("axios");
+// import express from 'express'
+// import cors from 'cors'
+// const express = require("express");
+// // const path = require('path');
+// // const cluster = require('cluster');
+// // const numCPUs = require('os').cpus().length;
+// const axios = require("axios");
 
-const app = express();
-let port = 3000;
-let hostname = "localhost";
+// const app = express();
+// let port = 3000;
+// let hostname = "localhost";
 
-app.use(express.static("../react-ui/public"));
-app.use(cors());
-app.use(express.json());
+// app.use(express.static("../react-ui/public"));
+// app.use(cors());
+// app.use(express.json());
 
-app.use("*", (req, res) => res.status(404).jons({error: "Not found"}))
+// app.use("*", (req, res) => res.status(404).jons({error: "Not found"}))
 
-app.listen(port, hostname, () => {
-  console.log(`Listening at: http://${hostname}:${port}`);
-});
+// app.listen(port, hostname, () => {
+//   console.log(`Listening at: http://${hostname}:${port}`);
+// });
 
-export default app
+// export default app
