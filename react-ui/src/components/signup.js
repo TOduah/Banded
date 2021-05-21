310af31d8e3cccc3b1413db5fd1b5d3348285158
tobenna <tjlite81@gmail.com>
2021-05-21 03:27:15 -0400

    updated sign up page

--- react-ui/src/components/signup.js
+++ react-ui/src/components/signup.js
@@ -1,9 +1,93 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Switch, Route, Link } from 'react-router-dom'
+import '../App.css'
+import Login from "./login";
 
-function SignUp(){
+const SignUp = props => {
+    // login template. will update for signup
+    // const initialUserState = {
+    //     name: "",
+    //     password: "",
+    //     id: ""
+    //   };
+    
+    //   const [user, setUser] = useState(initialUserState);
+    
+    //   const handleInputChange = event => {
+    //     const { name, value } = event.target;
+    //     setUser({ ...user, [name]: value });
+    //   };
+    
+    //   const signup = () => {
+    //     props.signup(user) // async function from App.js
+    //     props.history.push('/login'); //update url to / route
+    //   }
+    const signup = () => {
+        //
+    }
+    
+       
     return (
         <div className="App-header">
-            Page to Sign up.
+            <div>
+                <div className="form-group">
+                <label htmlFor="user">Username</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="name"
+                    // required
+                    // value={user.name}
+                    // onChange={handleInputChange}
+                    name="name"
+                />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="user">Email</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="email"
+                    // required
+                    // value={user.email}
+                    // onChange={handleInputChange}
+                    name="email"
+                />
+                </div>
+
+                <div className="form-group">
+                <label htmlFor="id">Password</label>
+                <input
+                    type="text"
+                    className="form-control"
+                    id="id"
+                    // required
+                    // value={user.id}
+                    // onChange={handleInputChange}
+                    name="id"
+                />
+                </div>
+
+                <button onClick={signup} className="btn btn-success">
+                    Sign Up
+                </button>
+                <div>
+                <Link to={"/login"} className="App-link">
+                        Login!
+                </Link>
+                </div>
+            </div>
+            {/* <div>
+                <Switch>
+                    <Route 
+                        exact path="/login"
+                        render={(props) => (
+                        <Login {...props} signup={signup} />
+                        )}
+                    />
+                </Switch>
+            </div> */}
         </div>
     );
 }
