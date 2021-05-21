874d966999c180c2c518ac7272658d74ae82b1e8
tobenna <tjlite81@gmail.com>
2021-05-21 08:10:30 -0400

    reverting to old index code

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+// file to access database
 import app from './server'
 import mongodb from 'mongodb'
 import dotenv from 'dotenv'
@@ -15,41 +16,41 @@ const PORT = process.env.PORT || 3000;
 
 
 
-// file to access database
+
 // const express = require('express');
-// const path = require('path');
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 // const axios = require("axios");
 
-// let app = express();
-// let port = 3000;
-// let hostname = "localhost";
+let app = express();
+let port = 3000;
+let hostname = "localhost";
 
-// app.use(express.static("../react-ui/public"));
-// app.use(cors());
-// app.use(express.json());
-// const isDev = process.env.NODE_ENV !== 'production';
-// const PORT = process.env.PORT || 5000;
+app.use(express.static("../react-ui/public"));
+app.use(cors());
+app.use(express.json());
+const isDev = process.env.NODE_ENV !== 'production';
+const PORT = process.env.PORT || 5000;
 
 // Multi-process to utilize all CPU cores.
-// if (!isDev && cluster.isMaster) {
-//   console.error(`Node cluster master ${process.pid} is running`);
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-//   });
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-// } else {
-//   const app = express();
+} else {
+  const app = express();
 
 //   // Priority serve any static files.
-//   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 //   // Answer API requests.
 //   app.get('/api', function (req, res) {
@@ -58,15 +59,15 @@ const PORT = process.env.PORT || 3000;
 //   });
 
 //   // All remaining requests return the React app, so it can handle routing.
-//   app.get('*', function(request, response) {
-//     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-//   });
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
 
-//   app.listen(PORT, function () {
-//     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
-//   });
-// }
+  app.listen(PORT, function () {
+    console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
+  });
+}
 
-// app.listen(port, hostname, () => {
-//   console.log(`Listening at: http://${hostname}:${port}`);
-// });
+app.listen(port, hostname, () => {
+  console.log(`Listening at: http://${hostname}:${port}`);
+});
