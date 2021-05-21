919411dc8ed02131021400f3b797ff1f73d3d26a
tobenna <tjlite81@gmail.com>
2021-05-21 12:40:12 -0400

    uncommented mongodb code

--- server/index.js
+++ server/index.js
@@ -69,12 +69,12 @@ if (!isDev && cluster.isMaster) {
   
 
   const uri = process.env.BANDER_DB_URI;
-  // mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-  // );
-  // const connection = mongoose.connection;
-  // connection.once('open', () => {
-  //   console.log("MongoDB database connection established successfully");
-  // })
+  mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
+  );
+  const connection = mongoose.connection;
+  connection.once('open', () => {
+    console.log("MongoDB database connection established successfully");
+  })
 
   // Answer API requests.
   app.get('/api', function (req, res) {
