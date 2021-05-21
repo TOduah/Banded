44398001edb1caf44f18566984bb062362646c5c
tobenna <tjlite81@gmail.com>
2021-05-21 10:11:42 -0400

    testing mongodb code

--- server/index.js
+++ server/index.js
@@ -7,12 +7,20 @@
 
 // // const PORT = process.env.PORT || 3000;
 
+const express = require('express');
+require('./db/mongoose');
+const User = require('./model/user');
 
+
+// const app = express();
+const port = process.env.PORT || 5000;
+
+// app.use(express.json());
 
 // const express = require('express');
-// const path = require('path');
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+const path = require('path');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 // // const cors = require("cors");
 // // const pool = require("./db");
 // // const axios = require("axios");
@@ -21,78 +29,47 @@
 // // let port = 3000;
 // // let hostname = "localhost";
 
-// // app.use(express.static("../react-ui/public"));
-// // app.use(cors());
-// // app.use(express.json());
-// const isDev = process.env.NODE_ENV !== 'production';
+
+const isDev = process.env.NODE_ENV !== 'production';
 // const PORT = process.env.PORT || 5000;
 
 // // Multi-process to utilize all CPU cores.
-// if (!isDev && cluster.isMaster) {
-//   console.error(`Node cluster master ${process.pid} is running`);
+if (!isDev && cluster.isMaster) {
+  console.error(`Node cluster master ${process.pid} is running`);
 
-//   // Fork workers.
-//   for (let i = 0; i < numCPUs; i++) {
-//     cluster.fork();
-//   }
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
-//   });
+  cluster.on('exit', (worker, code, signal) => {
+    console.error(`Node cluster worker ${worker.process.pid} exited: code ${code}, signal ${signal}`);
+  });
 
-// } else {
-//   const app = express();
+} else {
+  const app = express();
 
-// //   // Priority serve any static files.
-//   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
+//   // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
-// //   // Answer API requests.
-// //   app.get('/api', function (req, res) {
-// //     res.set('Content-Type', 'application/json');
-// //     res.send('{"message":"Hello from the custom server!"}');
-// //   });
+  // Answer API requests.
+  app.get('/api', function (req, res) {
+    res.set('Content-Type', 'application/json');
+    res.send('{"message":"Hello from the custom server!"}');
+  });
 
-// //   // All remaining requests return the React app, so it can handle routing.
-//   app.get('*', function(request, response) {
-//     response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-//   });
+//   // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
+  });
 
 //   app.listen(PORT, function () {
 //     console.error(`Node ${isDev ? 'dev server' : 'cluster worker '+process.pid}: listening on port ${PORT}`);
 //   });
-// }
-
-// // app.listen(port, hostname, () => {
-// //   console.log(`Listening at: http://${hostname}:${port}`);
-// // });
-
-const express = require('express');
-require('./db/mongoose');
-const User = require('./model/user');
-
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-app.use(express.json());
-
-app.get('/', async (req, res) => {
-  User.find({}).then((users) => {
-      res.send(users);
-  }).catch((e) => {
-      console.log('error: ' + e);
-  })
-});
-
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
-});
-
-app.get('/api', function (req, res) {
-  res.set('Content-Type', 'application/json');
-  res.send('{"message":"Hello from the custom server!"}');
-});
-
-app.listen(port, () => {
+  app.listen(port, () => {
     console.log('server is up on port ' + port);
-});
+  });
+}
+
+
+
